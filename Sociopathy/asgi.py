c4d40517fe78a61aa4d7d0705b19a1298a57d68e
fand1l <fand1l@outlook.com>
2026-03-08 14:16:53 +0200

    fix: reorder import statements in asgi.py for clarity

--- Sociopathy/asgi.py
+++ Sociopathy/asgi.py
@@ -15,9 +15,9 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from chat.routing import websocket_urlpatterns
-
 django_asgi_app = get_asgi_application()
+
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
 	{
